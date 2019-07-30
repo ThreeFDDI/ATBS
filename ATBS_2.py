46cb2c80b3d006940c2926b76f088fc6ce34549d
JT <JT@ThreeFDDI.com>
2019-07-30 17:28:47 -0500

    stuff

--- ATBS_2.py
+++ ATBS_2.py
@@ -1,5 +1,11 @@
+#!/usr/local/bin/python3
+
+import sys
+
 name = ""
 while name != "your name":
     print("Please type your name.")
     name = input()
+    if name == "exit":
+        sys.exit()
 print("Thank you!!")
