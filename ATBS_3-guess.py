adebb7c91363d135d18452305b1a357957f71bee
JT <JT@ThreeFDDI.com>
2019-07-30 22:22:23 -0500

    done with chap 3

--- ATBS_3-guess.py
+++ ATBS_3-guess.py
@@ -1,18 +1,24 @@
 #!/usr/local/bin/python3
 
 import random
+import sys
 
 def guess(secret_number, guesses):
-    print("You have {} tries left to guess the number...".format(6 - guesses))
+    print("You have {} tries left to guess the number...".format(7 - guesses))
     guesses += 1
     guess = int(input("What's your guess? "))
 
     if guess == secret_number:
-        print("Congratulations, you've guessed correctly!!")
+        print("\nCongratulations, you've guessed correctly!!\n")
+        sys.exit()
+
     elif guess < secret_number:
         print("Your guess ({}) was too low.".format(guess))
+        
     elif guess > secret_number:
         print("Your guess ({}) was too high.".format(guess))
+    
+    return guesses
 
 secret_number = random.randint(1,20)
 
@@ -22,4 +28,6 @@ print("\n*** Secret number is: {} ***\n".format(secret_number))
 print("I am thinking of a number between 1 and 20.")
 
 while guesses <= 6:
-    guess(secret_number,guesses)
+    guesses = guess(secret_number,guesses)
+
+print("\nSorry, you've run out of guesses.\n")
