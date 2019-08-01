237d9db4ccabb30faa9df062b1b8df76c7068d49
JT <JT@ThreeFDDI.com>
2019-07-31 22:11:40 -0500

    started tic-tac-toe

--- ATBS-5.py
+++ ATBS-5.py
@@ -1,2 +1,20 @@
 #!/usr/local/bin/python3
 
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+def currentBoard(theBoard):
+    print("Current\nboard:\n")
+    print(theBoard['top-L'] + " | " + theBoard['top-M'] + " | " + theBoard['top-R'])
+    print("-"*9)
+    print(theBoard['mid-L'] + " | " + theBoard['mid-M'] + " | " + theBoard['mid-R'])
+    print("-"*9)
+    print(theBoard['low-L'] + " | " + theBoard['low-M'] + " | " + theBoard['low-R'])
+    print("\n" + "~"*9 + "\n")
+
+currentBoard(theBoard)
+
+theBoard["low-R"] = "X"
+
+currentBoard(theBoard)
