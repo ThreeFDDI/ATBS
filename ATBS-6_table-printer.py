7c1a3143ceb6a371b3bb06bd79cc8bd15e9b089f
JT <JT@ThreeFDDI.com>
2019-08-03 09:13:14 -0500

    chap 6 practice project

--- ATBS-6_table-printer.py
+++ ATBS-6_table-printer.py
@@ -2,3 +2,7 @@
 
 # ATBS-6_table-printer.py prints formatted tables from lists
 
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
