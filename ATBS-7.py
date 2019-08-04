0ec368ad35e97fc27a7d073524a05dcb62b77d0d
JT <JT@ThreeFDDI.com>
2019-08-04 17:12:36 -0500

    more regex stuffs

--- ATBS-7.py
+++ ATBS-7.py
@@ -8,6 +8,24 @@ phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
 
 mo = phoneNumRegex.search('My number is 415-555-4242. Your number is 415-555-5858')
 
-print('Phone number found: ' + mo.group())
+print('Regex match: ' + mo.group())
 
+print("Regex findall: {}".format(phoneNumRegex.findall('My number is 415-555-4242. Your number is 415-555-5858')))
 
+phoneNumRegex2 = re.compile(r'(\d{3})-(\d{3}-\d{4})')
+
+mo2 = phoneNumRegex2.search('My number is 415-555-4242. Your number is 415-555-5858')
+
+print("Regex group 1: {}".format(mo2.group(1)))
+print("Regex group 2: {}".format(mo2.group(2)))
+print("Regex group 0: {}".format(mo2.group(0)))
+print("Regex group blank: {}".format(mo2.group()))
+print("Regex groups: {}".format(mo2.groups()))
+
+heroRegex = re.compile(r"Batman|Tina Fey")
+
+mo3 = heroRegex.search("Batman and Tina Fey.")
+print(mo3.group())
+
+mo3 = heroRegex.findall("Batman and Tina Fey.")
+print(mo3)
