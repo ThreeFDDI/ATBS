8f18a94ad2bfe256273f1c44a793f82c982764af
JT <JT@ThreeFDDI.com>
2019-08-04 09:57:20 -0500

    on to chap 7

---
+++ ATBS-7.py
@@ -0,0 +1,4 @@
+#!/usr/local/bin/python3
+
+# Chapter 7 - Regular expressions
+
--- test.py
+++ test.py
@@ -3,7 +3,9 @@
 def myfunction(mylist1):
     print(mylist1)
     mylist1=[0,1]
+    return mylist1
       
 mylist2=[2,3]
-myfunction (mylist2)
+mylist1 = myfunction (mylist2)
 print(mylist2)
+print(mylist1)
