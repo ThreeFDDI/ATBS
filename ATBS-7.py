ae72f1750479ba953a1d749e976ddc34491827d8
JT <JT@ThreeFDDI.com>
2019-08-04 11:10:43 -0500

    regex stuffs

--- ATBS-7.py
+++ ATBS-7.py
@@ -2,3 +2,15 @@
 
 # Chapter 7 - Regular expressions
 
+import re
+
+#phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+
+#mo = phoneNumRegex.search('My number is 415-555-4242. Your number is 415-555-5858')
+
+numbers = re.search((r'\d\d\d-\d\d\d-\d\d\d\d'), 'My number is 415-555-4242. Your number is 415-555-5858')
+
+print(numbers)
+
+#print('Phone number found: ' + mo.group())
+
