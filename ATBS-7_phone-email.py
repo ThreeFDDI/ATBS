f89646f74e550dc80d3af011232c1361dbc6d6af
JT <JT@ThreeFDDI.com>
2019-08-05 11:32:34 -0500

    regex project done

--- ATBS-7_phone-email.py
+++ ATBS-7_phone-email.py
@@ -5,8 +5,9 @@
 
 import pyperclip, re
 
+# phone regex search
 phoneRegex = re.compile(r'''(
-    (\d{3}|\d{3}\))?                # area code
+    (\d{3}|\(\d{3}\))?              # area code
     (\s|-|\.)?                      # separator
     (\d{3})                         # first 3 digits
     (\s|-|\.)                       # separator
@@ -14,8 +15,37 @@ phoneRegex = re.compile(r'''(
     (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
 )''', re.VERBOSE)
 
-# TODO: Create email regex.
+# email regex search
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+               # username
+    @                               # @ symbol
+    [a-zA-Z0-9.-]+                  # domain name
+    (\.[a-zA-Z]{2,4})               # TLD
+)''', re.VERBOSE)
 
-# TODO: Find matches in clipboard text.
+# import clipboard text
+text = str(pyperclip.paste())
 
-# TODO: Copy results to the clipboard.
+# initialize list of matches 
+matches = []
+
+# search text for phone numbers
+for groups in phoneRegex.findall(text):
+    # format the area code + phone number
+    phoneNum = "-".join([groups[1], groups[3], groups[5]])
+    # add extension if found
+    if groups[8] != "":
+        phoneNum += " x" + groups[8]
+    # add match to the list of matches
+    matches.append(phoneNum)
+
+# search text for email addresses
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+# print matches and send to clipboard
+if len(matches) > 0:
+    matches_out = "\n".join(matches)
+    pyperclip.copy(matches_out)
+    print("Copied to clipboard:")
+    print(matches_out)
