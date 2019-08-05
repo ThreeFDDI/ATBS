a623d61f95cf56687f6f170c2b5bea8eb8f1fc19
JT <JT@ThreeFDDI.com>
2019-08-05 15:50:09 -0500

    chap 8 stuffs

--- ATBS-8.py
+++ ATBS-8.py
@@ -2,3 +2,24 @@
 
 # ATBS-8.py
 
+import os
+
+print(os.path.abspath('.'))
+
+os.chdir("ATBS")
+
+print(os.path.abspath('.'))
+
+print(os.path.isabs('.'))
+
+print(os.path.isabs(os.path.abspath('.')))
+
+print(os.getcwd())
+
+os.makedirs("Chap8")
+
+os.chdir("Chap8")
+
+print(os.path.abspath('.'))
+
+print(os.path.basename('.'))
