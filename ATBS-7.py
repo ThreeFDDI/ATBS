5861694142bee9a8981c20fc9d9ddddf21f95761
JT <JT@ThreeFDDI.com>
2019-08-05 10:08:06 -0500

    more regex

--- ATBS-7.py
+++ ATBS-7.py
@@ -58,3 +58,18 @@ namesRegex = re.compile(r"Agent \w+")
 print(namesRegex.sub("REDACTED", "Agent Alice gave the secret documents to Agent Bob."))
 agentNamesRegex = re.compile(r"Agent (\w)\w*")
 print(agentNamesRegex.sub(r"\1****", "Agent Alice told Agent Carol that Agent Eve knew Agent Bob was a double agent."))
+
+# Managing Complex Regexes
+phoneRegex = re.compile(r'((\d{3}|\(\d{3}\))?(\s|-|\.)?\d{3}(\s|-|\.)\d{4}(\s*(ext|x|ext.)\s*\d{2,5})?)')
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?            # area code
+    (\s|-|\.)?                    # separator
+    \d{3}                         # first 3 digits
+    (\s|-|\.)                     # separator
+    \d{4}                         # last 4 digits
+    (\s*(ext|x|ext.)\s*\d{2,5})?  # extension
+    )''', re.VERBOSE) # re.VERBOSE ignores whitespace and comments
+
+
+
+
