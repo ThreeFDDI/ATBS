8d890bcdb5b22555e60d866546aad87caee0055f
JT <JT@ThreeFDDI.com>
2019-08-05 20:02:32 -0500

    files and stuffs

--- ATBS-8.py
+++ ATBS-8.py
@@ -16,10 +16,23 @@ print(os.path.isabs(os.path.abspath('.')))
 
 print(os.getcwd())
 
-os.makedirs("Chap8")
+#os.makedirs("Chap8")
 
 os.chdir("Chap8")
 
 print(os.path.abspath('.'))
 
-print(os.path.basename('.'))
+print(os.path.basename("/Users/JT/JTGIT/ATBS/Chap8/test.txt"))
+
+print(os.path.dirname("/Users/JT/JTGIT/ATBS/Chap8/test.txt"))
+
+print(os.listdir("/Users/JT/JTGIT/ATBS/"))
+
+totalSize = 0
+path = "/Users/JT/JTGIT/ATBS/"
+for file in os.listdir(path):
+    totalSize = totalSize + os.path.getsize(os.path.join(path, file))
+
+print("Total size of all files: {}".format(totalSize))
+
+helloFile = open('hello.txt')
