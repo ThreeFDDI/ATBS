a9a6eaf6ab8a32e35e269d1169cb11b034d8ceb3
JT <JT@ThreeFDDI.com>
2019-08-05 21:00:28 -0500

    file stuffffz

--- ATBS-8.py
+++ ATBS-8.py
@@ -38,3 +38,17 @@ print("Total size of all files: {}".format(totalSize))
 helloFile = open('hello.txt')
 
 print(helloFile.read())
+
+baconFile = open('bacon.txt', 'w')
+
+baconFile.write('Hello world!\n')
+
+baconFile.close()
+
+baconFile = open('bacon.txt', 'a')
+baconFile.write('Bacon is not a vegetable.')
+baconFile.close()
+baconFile = open('bacon.txt')
+content = baconFile.read()
+baconFile.close()
+print(content)
