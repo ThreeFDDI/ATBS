6427b8a4c0e3686dd6fd6b8cf497f533f66f5d28
JT <JT@ThreeFDDI.com>
2019-08-11 08:10:19 -0500

    clippy stuff

--- ATBS-8_multiclipboard.py
+++ ATBS-8_multiclipboard.py
@@ -10,8 +10,17 @@ import shelve, pyperclip, sys
 
 mcbShelf = shelve.open("mcb")
 
-# TODO - save clipboard contents
-
-# TODO - lisr keywords and load content
+# save clipboard contents
+if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
+    print("Saving....")
+    mcbShelf[sys.argv[2]] = pyperclip.paste()
+elif len(sys.argv) == 2:
+    pass
+    
+    # list keywords and load content
+    if sys.argv[1].lower() == "list":
+        pyperclip.copy(str(list(mcbShelf.keys())))
+    elif sys.argv[1] in mcbShelf:
+        pyperclip.copy(mcbShelf[sys.argv])
 
 mcbShelf.close()
