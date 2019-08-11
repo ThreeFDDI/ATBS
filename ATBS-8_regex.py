048b7d213070455816e01c26bf8369423d9a818c
JT <JT@ThreeFDDI.com>
2019-08-11 09:24:18 -0500

    regex stuff

--- ATBS-8_regex.py
+++ ATBS-8_regex.py
@@ -1,1 +1,11 @@
 #!/usr/local/bin/python3
+
+import os
+
+# set path to be searched
+path = "/Users/JT/JTGIT/ATBS/ATBS-8_regex_dir"
+
+# list files in directory
+files = os.listdir(path)
+
+print(files)
