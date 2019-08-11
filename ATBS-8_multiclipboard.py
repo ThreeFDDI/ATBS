95d46bd08dbc78a93ce00ebc4c6fe110db62e9fe
JT <JT@ThreeFDDI.com>
2019-08-11 07:52:18 -0500

    more pyperclip

--- ATBS-8_multiclipboard.py
+++ ATBS-8_multiclipboard.py
@@ -1,1 +1,17 @@
 #!/usr/local/bin/python3
+
+# multiclipboard saves and loads text to the clipboard
+
+# Usage: py ATBS-8_multiclipboard.pyw save <keyword> saves clipboard to keyword
+# Usage: py ATBS-8_multiclipboard.pyw  <keyword> loads keyword to clipboard
+# Usage: py ATBS-8_multiclipboard.pyw list loads all keywords to clipboard
+
+import shelve, pyperclip, sys
+
+mcbShelf = shelve.open("mcb")
+
+# TODO - save clipboard contents
+
+# TODO - lisr keywords and load content
+
+mcbShelf.close()
