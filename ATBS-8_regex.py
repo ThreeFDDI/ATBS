f49e9bed06c591866142b16ba32df34a2111b640
JT <JT@ThreeFDDI.com>
2019-08-11 09:34:38 -0500

    regex file search

--- ATBS-8_regex.py
+++ ATBS-8_regex.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3
 
-import os
+import os, re
 
 # set path to be searched
 path = "/Users/JT/JTGIT/ATBS/ATBS-8_regex_dir"
@@ -8,4 +8,10 @@ path = "/Users/JT/JTGIT/ATBS/ATBS-8_regex_dir"
 # list files in directory
 files = os.listdir(path)
 
-print(files)
+#print(files)
+
+for file in files:
+    if ".txt" in file: 
+        with open(path + "/" + file) as file:
+            data = file.read()
+            print(data)
