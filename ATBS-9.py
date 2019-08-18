dc11684a2ffe29eed3eea87eba686cf652a00678
JT <JT@ThreeFDDI.com>
2019-08-18 14:38:32 -0500

    filessss

--- ATBS-9.py
+++ ATBS-9.py
@@ -13,3 +13,5 @@ print(os.getcwd())
 shutil.copy('/Users/JT/JTGIT/ATBS/ATBS-9_dir1/file1', '/Users/JT/JTGIT/ATBS/ATBS-9_dir1/file2')
 shutil.copy('/Users/JT/JTGIT/ATBS/ATBS-9_dir1/file1', '/Users/JT/JTGIT/ATBS/ATBS-9_dir2/')
 
+os.unlink('/Users/JT/JTGIT/ATBS/ATBS-9_dir1/file1')
+shutil.move('/Users/JT/JTGIT/ATBS/ATBS-9_dir2/file1', '/Users/JT/JTGIT/ATBS/ATBS-9_dir1/')
