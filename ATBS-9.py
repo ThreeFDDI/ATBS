ee671bf3dc2573b960c6ce2a772a8bbff0b4459d
JT <JT@ThreeFDDI.com>
2019-08-18 16:53:14 -0500

    file movers

--- ATBS-9.py
+++ ATBS-9.py
@@ -13,5 +13,17 @@ print(os.getcwd())
 shutil.copy('/Users/JT/JTGIT/ATBS/ATBS-9_dir1/file1', '/Users/JT/JTGIT/ATBS/ATBS-9_dir1/file2')
 shutil.copy('/Users/JT/JTGIT/ATBS/ATBS-9_dir1/file1', '/Users/JT/JTGIT/ATBS/ATBS-9_dir2/')
 
-os.unlink('/Users/JT/JTGIT/ATBS/ATBS-9_dir1/file1')
-shutil.move('/Users/JT/JTGIT/ATBS/ATBS-9_dir2/file1', '/Users/JT/JTGIT/ATBS/ATBS-9_dir1/')
+#os.unlink('/Users/JT/JTGIT/ATBS/ATBS-9_dir1/file1')
+shutil.move('/Users/JT/JTGIT/ATBS/ATBS-9_dir2/file1', '/Users/JT/JTGIT/ATBS/ATBS-9_dir1/file1')
+
+print("\nFiles in dir1:")
+listOfFiles = os.listdir('/Users/JT/JTGIT/ATBS/ATBS-9_dir1/')
+
+for i in listOfFiles:
+    print(i)
+    
+print("\nFiles in dir2:")
+listOfFiles = os.listdir('/Users/JT/JTGIT/ATBS/ATBS-9_dir2/')
+
+for i in listOfFiles:
+    print(i)
