73b6beb9a7eae8af2c6ca04db8a579572442f77b
JT <JT@ThreeFDDI.com>
2019-08-18 21:49:01 -0500

    gmaps stuffs

--- ATBS-11_gmaps.py
+++ ATBS-11_gmaps.py
@@ -1,5 +1,16 @@
 #! /usr/local/bin/python3
 
-import webbrowser
+# mapit:
+# launches map in the browser using an address 
+# from the command line or clipboard
 
-webbrowser.open('http://inventwithpython.com/')
+import webbrowser, pyperclip, sys
+
+if len(sys.argv) > 1:
+    # get address from command line
+    address = " ".join(sys.argv[1:])
+else:
+    # get address from clipboard   
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
