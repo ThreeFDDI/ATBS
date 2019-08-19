509cca5e6591416965b239b283ca068fbda1df39
JT <JT@ThreeFDDI.com>
2019-08-19 18:57:40 -0500

    xkcd progress

--- ATBS-11_xkcd.py
+++ ATBS-11_xkcd.py
@@ -2,3 +2,28 @@
 
 # download ALL the xkcd
 
+import requests, bs4, os
+
+# starting url for download
+url = 'http://xkcd.com'
+
+# folder structure for comics
+os.makedirs("ATBS-11_xkcd", exist_ok=True)
+
+while not url.endswith("#"):
+    url = 'http://xkcd.com/#'
+    continue
+
+    # TODO download page
+
+    # TODO find the next URL
+
+    # TODO download the image
+
+    # TODO get the prev button's url
+
+
+
+
+
+print("Done.")
