d5809403f0460584b0ee395dcc4fb67cb150eeae
JT <JT@ThreeFDDI.com>
2019-08-19 22:12:54 -0500

    sheetz

--- ATBS-12.py
+++ ATBS-12.py
@@ -7,3 +7,13 @@ os.chdir("/Users/JT/JTGIT/ATBS")
 wb = openpyxl.load_workbook("ATBS-12_example.xlsx")
 
 print(type(wb))
+
+sheet = wb.get_sheet_by_name("Sheet3")
+
+print(sheet)
+
+print(type(sheet))
+
+#print(dir(sheet))
+
+print(sheet.title)
