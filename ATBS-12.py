79beb40d7bd1606d7c8a4b1da5f00ec95d65e615
JT <JT@ThreeFDDI.com>
2019-08-20 08:53:26 -0500

    celly cel

--- ATBS-12.py
+++ ATBS-12.py
@@ -1,6 +1,7 @@
 #! /usr/local/bin/python3
 
 import openpyxl, os
+from openpyxl.utils import get_column_letter, column_index_from_string
 
 os.chdir("/Users/JT/JTGIT/ATBS")
 
@@ -14,10 +15,31 @@ print(sheet)
 
 print(type(sheet))
 
-#print(dir(sheet))
-
 print(sheet.title)
 
 anotherSheet = wb.active
 
 print(anotherSheet)
+
+print(anotherSheet.title)
+
+#print(dir(anotherSheet))
+
+a = anotherSheet["A1"]
+b = anotherSheet["B1"]
+c = anotherSheet["C1"]
+
+print(a.value)
+print(b.value)
+print(c.value)
+
+print()
+
+print(b.row)
+print(b.column)
+print(b.coordinate)
+
+print(get_column_letter(27))
+print(get_column_letter(900))
+print(column_index_from_string('AA'))
+print(column_index_from_string('AHP'))
