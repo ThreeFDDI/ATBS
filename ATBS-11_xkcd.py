10acb95af4abb0566323c8c46424fcf1f797137f
JT <JT@ThreeFDDI.com>
2019-08-19 19:30:04 -0500

    xkcd stuffs

--- ATBS-11_xkcd.py
+++ ATBS-11_xkcd.py
@@ -5,7 +5,7 @@
 import requests, bs4, os
 
 # starting url for download
-url = 'http://xkcd.com'
+url = "http://xkcd.com"
 
 # folder structure for comics
 os.makedirs("ATBS/ATBS-11_xkcd", exist_ok=True)
@@ -14,28 +14,63 @@ while not url.endswith("#"):
 
     # download the page
     print(f"Downloading page {url}...")
+    
     # HTTP GET for url
     res = requests.get(url)
+    
     # raise exception for error codes
     res.raise_for_status()
-
+    
     # soup it up
     soup = bs4.BeautifulSoup(res.text, features="lxml")
-
-    #print(soup)
-
+    
     # Find the URL of the comic image
     comicElem = soup.select('#comic img')
+    
+    # error if no comic image found
+    if comicElem == []:
+        print("Could not find comic image.")
+    
+    # try to download comic image
+    else:
+        try:
+            # create current comic url from soup select
+            comicURL = "http:" + comicElem[0].get("src")
+            
+            # download image
+            print(f"Downloading image {comicURL}...")
+            res = requests.get(comicURL)
+            
+            # raise exception for error codes
+            res.raise_for_status()
 
-    print(comicElem)
+        # skip pages without a comic
+        except requests.exceptions.MissingSchema:
+            # get link for Prev button
+            prevLink = soup.select('a[rel="prev"]')[0]
+            # set url to go to previous page
+            url = "http://xkcd.com" + prevLink.get("href")
+            continue
+    
+    # Save the image to ATBS/ATBS-11_xkcd directory
+    imageFile = open(os.path.join("ATBS/ATBS-11_xkcd", os.path.basename(comicURL)), "wb")
+    
+    # do magic
+    for chunk in res.iter_content(100000):
+        imageFile.write(chunk)
+    
+    # close image file
+    imageFile.close()
+
+    # get link for Prev button
+    prevLink = soup.select('a[rel="prev"]')[0]
+    
+    # set url to go to previous page
+    url = "http://xkcd.com" + prevLink.get("href")
+            
 
 
-    # TODO find the next URL
 
-    # TODO download the image
-
-    # TODO get the prev button's url
-    url = 'http://xkcd.com/#'
     continue
 
 
