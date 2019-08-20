e3d8d97a2cebd00df7f4127ebd1cdb2570f2f95e
JT <JT@ThreeFDDI.com>
2019-08-19 19:10:17 -0500

    more atbs

--- ATBS-11_xkcd.py
+++ ATBS-11_xkcd.py
@@ -8,19 +8,35 @@ import requests, bs4, os
 url = 'http://xkcd.com'
 
 # folder structure for comics
-os.makedirs("ATBS-11_xkcd", exist_ok=True)
+os.makedirs("ATBS/ATBS-11_xkcd", exist_ok=True)
 
 while not url.endswith("#"):
-    url = 'http://xkcd.com/#'
-    continue
 
-    # TODO download page
+    # download the page
+    print(f"Downloading page {url}...")
+    # HTTP GET for url
+    res = requests.get(url)
+    # raise exception for error codes
+    res.raise_for_status()
+
+    # soup it up
+    soup = bs4.BeautifulSoup(res.text, features="lxml")
+
+    #print(soup)
+
+    # Find the URL of the comic image
+    comicElem = soup.select('#comic img')
+
+    print(comicElem)
+
 
     # TODO find the next URL
 
     # TODO download the image
 
     # TODO get the prev button's url
+    url = 'http://xkcd.com/#'
+    continue
 
 
 
