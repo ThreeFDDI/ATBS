69e7b4150109d1895c03b54f792ebf29b8b695c8
JT <JT@ThreeFDDI.com>
2019-08-20 18:21:13 -0500

    produce sales

---
+++ ATBS-12_produce.py
@@ -0,0 +1,12 @@
+#! /usr/local/bin/python3
+
+import openpyxl, pprint, os
+
+# change directory
+os.chdir("/Users/JT/JTGIT/ATBS")
+
+print("Creating workbook...")
+# create new workbook
+
+wb = openpyxl.Workbook()
+
