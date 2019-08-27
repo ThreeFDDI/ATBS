04baf4f4b9741e4b58c1406b9340d73d0cb56592
JT <JT@ThreeFDDI.com>
2019-08-27 14:20:03 -0500

    csv stuff

--- ATBS-14.py
+++ ATBS-14.py
@@ -6,3 +6,27 @@
 
 import csv, json
 
+#exFile = open("ATBS/ATBS-14_example.csv")
+#exReader = csv.reader(exFile)
+
+exReader = csv.reader(open("ATBS/ATBS-14_example.csv"))
+
+exData = list(exReader)
+
+print()
+print(exData)
+print()
+
+for i in exData:
+    print(i)
+
+print()
+print(exData[1])
+print(exData[5])
+print(exData[5][1])
+print()
+
+for i in exData[5]:
+    print(i)
+
+outputFile = open("ATBS/ATBS-14_output.csv", "w", newline="")
