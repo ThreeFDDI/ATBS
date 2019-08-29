0907316e5f823a8a9e39fdea43097c9a9143a642
JT <JT@ThreeFDDI.com>
2019-08-29 18:43:51 -0500

    super stopwatch

--- ATBS-15_stopwatch.py
+++ ATBS-15_stopwatch.py
@@ -17,3 +17,16 @@ print("Started")
 startTime = time.time()
 lastTime = startTime
 lapNum = 1
+
+try:
+    while True:
+        input()
+        lapTime = round(time.time() - lastTime, 2)
+        totalTime = round(time.time() - startTime, 2)
+        print(f"Lap {lapNum}: {totalTime} {lapTime}")
+        lapNum += 1
+        lastTime = time.time()
+        
+except KeyboardInterrupt:
+    print("\nDone.")
+    pass
