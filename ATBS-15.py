407eadc60c17ecf119237de8bf1e8687f9732661
JT <JT@ThreeFDDI.com>
2019-08-28 23:45:31 -0500

    sleepy

--- ATBS-15.py
+++ ATBS-15.py
@@ -11,12 +11,20 @@ import time
 def calcProd():
        # Calculate the product of the first 100,000 numbers.
        product = 1
-       for i in range(1, 100000):
+       for i in range(1, 1000000):
            product = product * i
        return product
 
-startTime = time.time()
+startTime = round(time.time())
 prod = calcProd()
-endTime = time.time()
+endTime = round(time.time())
+#print(prod)
 print('The result is %s digits long.' % (len(str(prod))))
 print('Took %s seconds to calculate.' % (endTime - startTime))
+
+for i in range(5):
+    print("Tick")
+    time.sleep(1)
+    print("Tock")
+    time.sleep(1)
+
