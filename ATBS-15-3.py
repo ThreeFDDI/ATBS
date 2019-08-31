2a532db0d65e0f3687e2d9d188e1a73315bb4b98
JT <JT@ThreeFDDI.com>
2019-08-31 12:06:32 -0500

    stuff

--- ATBS-15-3.py
+++ ATBS-15-3.py
@@ -3,3 +3,10 @@
 # Chapter 15 – Keeping Time, Scheduling Tasks, and Launching Programs
 
 # ATBS-15-3.py 
+
+import time, datetime
+
+startTime = datetime.datetime(2029, 10, 31)
+while datetime.datetime.now() < startTime:
+    print("SLEEP")
+    time.sleep(10)
