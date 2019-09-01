dc4b4179203e4e74a6e6a63e7dc1212e6babe3f6
JT <JT@ThreeFDDI.com>
2019-09-01 17:40:03 -0500

    smtp sucks

--- ATBS-16.py
+++ ATBS-16.py
@@ -4,4 +4,16 @@
 
 # ATBS-16.py 
 
+import email, smtplib, ssl
 
+smtpObj = smtplib.SMTP("smtp.gmail.com", 587)
+
+smtpObj.ehlo()
+
+smtpObj.starttls()
+
+smtpObj.login('MySMS5858', '5232iwEY3mrExC6xbn8k')
+
+smtpObj.sendmail('MySMS5858@gmail.com', 'foosyou@gmail.com', 'Subject: So long.')
+
+#smtpObj.quit()
