b8c4fa6e037811f259f8a60bc32a949f4b625217
JT <JT@ThreeFDDI.com>
2019-09-02 17:51:20 -0500

    kitty

--- ATBS-17.py
+++ ATBS-17.py
@@ -4,6 +4,21 @@
 
 # ATBS-17.py 
 
-from PIL import ImageColor
+from PIL import ImageColor, Image
 
 print(ImageColor.getcolor('red', 'RGBA'))
+
+catIm = Image.open('ATBS/ATBS-17_zophie.png')
+
+print()
+print(catIm)
+print(type(catIm))
+print(catIm.size)
+print(catIm.width)
+print(catIm.height)
+print(catIm.filename)
+print(catIm.format)
+print(catIm.format_description)
+catIm.save('ATBS/ATBS-17_zophie2.png')
+
+
