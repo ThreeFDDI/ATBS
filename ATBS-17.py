c33e487cdf86165ddcf21ca1f56add127453516d
JT <JT@ThreeFDDI.com>
2019-09-02 20:16:29 -0500

    kitty kitty

--- ATBS-17.py
+++ ATBS-17.py
@@ -22,11 +22,20 @@ print(catIm.format_description)
 catIm.save('ATBS/ATBS-17_zophie.jpg')
 print()
 
+# crop face and save as new file
 croppedIm = catIm.crop((335, 345, 565, 560))
 croppedIm.save('ATBS/ATBS-17_zophie-cropped.png')
 
+# copy file
 catCopyIm = catIm.copy()
-
+# paste cropped face into file
 catCopyIm.paste(croppedIm, (0, 0))
 catCopyIm.paste(croppedIm, (400, 500))
+# save file
 catCopyIm.save('ATBS/ATBS-17_zophie-pasted.png')
+
+catImWidth, catImHeight = catIm.size
+faceImWidth, faceImHeight = croppedIm.size
+catCopyTwo = catIm.copy()
+
+
