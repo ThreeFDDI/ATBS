6e1df6f94961ad7e1d72d79e74d1f9b21ea0216c
JT <JT@ThreeFDDI.com>
2019-09-24 15:29:32 -0500

    ATBS3 eightball

---
+++ ATBS-3._eight-ball.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+
+import random
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return "It is certain."
+    elif answerNumber == 2:
+        return "It is decidely so."
+    elif answerNumber == 3:
+        return "Yes."
+    elif answerNumber == 4:
+        return "Reply hazy try again."
+    elif answerNumber == 5:
+        return "Ask again later"
+    elif answerNumber == 6:
+        return "Concentrate and ask again."
+    elif answerNumber == 7:
+           return "My reply is NO."
+    elif answerNumber == 8:
+           return "Outlook not so good."
+    elif answerNumber == 9:
+           return "Very doubtful."
+
+
+print("\nThe answer to your question is: {}".format(getAnswer(random.randint(1,9))))
